02d7266d36a41e81d438b06dbd09bfab3a2f1019
Phil Fernandez <philvfernandez@gmail.com>
2020-01-20 11:58:29 -0700

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Phil");
         owner1.setLastName("Fernandez");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Isabella");
         owner2.setLastName("Fernandez");
         ownerService.save(owner2);
@@ -36,19 +34,15 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Cadence");
         vet1.setLastName("Fernandez");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Coco");
         vet2.setLastName("Burton");
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
-
-
     }
 }
