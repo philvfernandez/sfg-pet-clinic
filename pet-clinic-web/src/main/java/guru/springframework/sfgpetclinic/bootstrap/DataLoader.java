551c40f754c63c955c0f76bb9e1ff416612f90d7
Phil Fernandez <philvfernandez@gmail.com>
2020-01-26 13:09:24 -0700

    Added Owners enhancement with contact info and Pets to data loader startup.  Closes #33

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -37,12 +40,35 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Phil");
         owner1.setLastName("Fernandez");
+        owner1.setAddress("9230 E 52nd Drive");
+        owner1.setCity("Denver");
+        owner1.setTelephone("720-201-9949");
         ownerService.save(owner1);
+
+        Pet philsPet = new Pet();
+        philsPet.setPetType(saveDogPetType);
+        philsPet.setOwner(owner1);
+        philsPet.setBirthDate(LocalDate.now());
+        philsPet.setName("Cadence");
+        owner1.getPets().add(philsPet);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Isabella");
         owner2.setLastName("Fernandez");
+        owner2.setAddress("9230 E 52nd Dr");
+        owner2.setCity("Denver");
+        owner2.setTelephone("1221248848");
+
+        Pet issysCat = new Pet();
+        issysCat.setPetType(saveCatPetType);
+        issysCat.setOwner(owner2);
+        issysCat.setBirthDate(LocalDate.now());
+        issysCat.setName("Crazy Cat");
+        owner2.getPets().add(issysCat);
+
         ownerService.save(owner2);
+
+
 
         System.out.println("Loaded Owners....");
 
